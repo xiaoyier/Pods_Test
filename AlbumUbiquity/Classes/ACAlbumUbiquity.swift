09f28cc6d5df46db87fa4dcb4e6e59ec46b78865
AllConnected <AC@AllConnecteddeMacBook-Pro.local>
2017-08-22 11:38:27 +0800

    private library init

---
+++ AlbumUbiquity/Classes/ACAlbumUbiquity.swift
@@ -0,0 +1,13 @@
+//
+//  ACAlbumUbiquity.swift
+//  Pods
+//
+//  Created by AllConnected on 2017/8/22.
+//
+//
+
+import UIKit
+
+class ACAlbumUbiquity: NSObject {
+
+}
---
+++ Example/Pods/Target Support Files/AlbumUbiquity/AlbumUbiquity-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AlbumUbiquity : NSObject
+@end
+@implementation PodsDummy_AlbumUbiquity
+@end
